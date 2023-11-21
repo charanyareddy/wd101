291db7d25a1a395a584d80f940db80c13c0fa63c
charanyareddy <118651744+charanyareddy@users.noreply.github.com>
2023-11-21 19:16:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,75 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const registrationForm = document.getElementById('registrationForm');
-  const dataTableBody = document.querySelector('#dataTable tbody');
+document.addEventListener("DOMContentLoaded", function () {
+    // Load entries from localStorage
+    loadTableEntries();
 
-  registrationForm.addEventListener('submit', function (event) {
-    event.preventDefault();
+    document.getElementById("registrationForm").addEventListener("submit", function(event) {
+        event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const dob = document.getElementById('dob').value;
-    const acceptTerms = document.getElementById('acceptTerms').checked;
+        // Get form values
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+        var dob = document.getElementById("dob").value;
+        var acceptTerms = document.getElementById("acceptTerms").checked;
 
-    // Additional validation for age between 18 and 55
-    const dobDate = new Date(dob);
-    const today = new Date();
-    const age = today.getFullYear() - dobDate.getFullYear();
+        // Validate email and age
+        if (!isValidEmail(email) || !isValidAge(dob)) {
+            alert("Invalid email address or age!");
+            return;
+        }
 
-    if (age < 18 || age > 55) {
-      alert('You must be between 18 and 55 years old to register.');
-      return;
+        // Add a new row to the table
+        var table = document.getElementById("userTable").getElementsByTagName('tbody')[0];
+        var newRow = table.insertRow(table.rows.length);
+        var cells = [name, email, password, dob, acceptTerms];
+
+        for (var i = 0; i < cells.length; i++) {
+            var cell = newRow.insertCell(i);
+            cell.innerHTML = cells[i];
+        }
+
+        // Save entry to localStorage
+        saveTableEntry(name, email, password, dob, acceptTerms);
+
+        // Clear the form
+        document.getElementById("registrationForm").reset();
+    });
+
+    function isValidEmail(email) {
+        // Basic email validation
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
     }
 
-    // Display entered data in the table
-    const newRow = dataTableBody.insertRow();
-    newRow.innerHTML = `<td>${name}</td><td>${email}</td><td>${password}</td><td>${dob}</td><td>${acceptTerms ? 'Yes' : 'No'}</td>`;
+    function isValidAge(dob) {
+        // Validate age between 18 and 55
+        var birthDate = new Date(dob);
+        var currentDate = new Date();
+        var age = currentDate.getFullYear() - birthDate.getFullYear();
+        return age >= 18 && age <= 55;
+    }
 
-    // Clear the form
-    registrationForm.reset();
-  });
+    function saveTableEntry(name, email, password, dob, acceptTerms) {
+        // Save entry to localStorage
+        var entry = { name, email, password, dob, acceptTerms };
+        var entries = JSON.parse(localStorage.getItem("entries")) || [];
+        entries.push(entry);
+        localStorage.setItem("entries", JSON.stringify(entries));
+    }
+
+    function loadTableEntries() {
+        // Load entries from localStorage
+        var entries = JSON.parse(localStorage.getItem("entries")) || [];
+        var table = document.getElementById("userTable").getElementsByTagName('tbody')[0];
+
+        entries.forEach(function(entry) {
+            var newRow = table.insertRow(table.rows.length);
+            var cells = [entry.name, entry.email, entry.password, entry.dob, entry.acceptTerms];
+
+            for (var i = 0; i < cells.length; i++) {
+                var cell = newRow.insertCell(i);
+                cell.innerHTML = cells[i];
+            }
+        });
+    }
 });
